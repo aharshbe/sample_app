8acf8fba47c0709435c5748b9cca896818aacbd5
EC2 Default User <ec2-user@ip-172-31-24-182.us-east-2.compute.internal>
2018-09-14 01:51:14 +0000

    adding ccsf version

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root 'ccsf_rails#index'
+  get 'ccsf_rails/assignmentsclear'
+  get 'ccsf_rails/assignments'
+  get 'ccsf_rails/index'
+  get 'ccsf_rails/links'
+  get 'ccsf_rails/about'
+
   get  'static_pages/home'
   get  'static_pages/help'
   get  'static_pages/about'
